a4667e1128ac77a750716406cc91efc90ca82fec
Angelo Mantellini <manangel@cisco.com>
2019-05-24 12:04:29 +0200

    add libparc

---
+++ libparc.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Libparc < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/FDio/cicn/archive/cframework/master.zip"
+  version "1.0"
+  sha256 ""
+  depends_on "cmake" => :build
+  depends_on "openssl@1.1"
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cmake", "libparc", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test libparc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
